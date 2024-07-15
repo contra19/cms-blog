15c821a3257b0109bb9b90a0de2b9dc63dfffe74
Robert Wisniewski <contra19@gmail.com>
2024-07-14 22:34:57 -0400

    minor bug fix for date

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -3,7 +3,7 @@ const dateHelper = require('handlebars');
 
 // Handlebars helper for date formatting
 dateHelper.registerHelper('formatDate', function(date) {
-    return format(new Date(date), 'MMMM Do yyyy, h:mm:ss a');
+    return format(new Date(date), 'MMMM do yyyy, h:mm:ss a');
 });
 
 module.exports = dateHelper;
